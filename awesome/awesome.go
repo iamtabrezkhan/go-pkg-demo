8c41a7efceb0cf782700bec8b35b7f6aa1df04c1
Tabrez Khan <khan.t.29@gmail.com>
2020-06-25 17:07:45 +0530

    first commit

---
+++ awesome/awesome.go
@@ -0,0 +1,7 @@
+package awesome
+
+import "fmt"
+
+func PrintAwesome() {
+	fmt.Println("I am awesome!")
+}
---
+++ greet/greet.go
@@ -0,0 +1,11 @@
+package greet
+
+import "fmt"
+
+func GreetMorning() {
+	fmt.Println("Hey, Good Morning!")
+}
+
+func GreetEvening() {
+	fmt.Println("Hey, Good Evening!")
+}
